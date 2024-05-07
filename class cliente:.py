2f5dedb9d25254ac43314a0f0c039d2f940c71fd
mavifroiss <130071761+mavifroiss@users.noreply.github.com>
2024-05-07 10:30:34 -0300

    Add files via upload
    aula de hoje 07/05/2024

---
+++ class cliente:.py
@@ -0,0 +1,34 @@
+class cliente:
+    def __init__(self,nome,anoNascimento,sexo,saldo,endereco,ativo):
+        self.nome = nome 
+        self.anoNascimento = anoNascimento
+        self.sexo = sexo
+        self.saldo = saldo 
+        self.endereco = endereco
+        self.ativo = ativo
+
+    def imprimir(self):
+        print("nome", self.nome)
+        print("ano", self.anoNascimento)
+        print("sexo", self.sexo)
+        print("saldo", self.saldo)
+        print("endereco", self.endereco)
+        print("ativo", self.ativo)
+
+    def ValidaClilente(self):  
+        if self.ativo:
+            print("O cliente", self.nome, "está ativo")   
+        else:  
+            print("O cliente", self.nome, "não esta ativo.")
+        
+objeto = cliente("Maria", 2006, "F", 760, "Av. Santos Dumont", True)
+objeto2 = cliente("Maria", 2006, "F", -760, "Av. Santos Dumont", True)
+objeto3 = cliente("Maria", 2006, "F", 760, "Av. Santos Dumont", False)
+
+#objeto.imprimir() 
+#objeto2.imprimir()                  
+#objeto3.imprimir()
+
+objeto.ValidaClilente()                                                                  
+objeto2.ValidaClilente()                                                                  
+objeto3.ValidaClilente()                                                                  
